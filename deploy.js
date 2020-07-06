03c38ceb5f3c33a951625253e44e8017e8695d64
acenturyandabit <steeven.liu2@gmail.com>
2020-07-07 08:31:20 +1000

    'auto-blog-update'

--- deploy.js
+++ deploy.js
@@ -34,13 +34,12 @@ for (let f of filesList) {
     let copy = JSON.parse(JSON.stringify(templateLines));
     let selectedFileLines = String(fs.readFileSync("entries_md/" + f)).split(/\r?\n/g);
     selectedFileLines.unshift(insertionLine + 1, 0);// add 0 to args so that we splice away nothing
-    copy.splice.apply(copy, selectedFile);
-    copy.splice.apply(copy, [0, titleInsertionLine + 1, selectedFileLines[0].slice(1)]);
+    copy.splice.apply(copy, selectedFileLines);
+    copy.splice.apply(copy, [titleInsertionLine + 1, 0, selectedFileLines[2].slice(1)]);
     fs.mkdirSync(`entries/${f.slice(0, f.length - 3)}`);
     fs.writeFileSync(`entries/${f.slice(0, f.length - 3)}/index.html`, copy.join("\n"));
 }
-/*
+
 execSync("git add .")
 execSync("git commit -m 'auto-blog-update'");
 execSync("git push");
-*/
