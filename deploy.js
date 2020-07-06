748c1cab9bdf66cd298971aaa50696d3f26db383
acenturyandabit <steeven.liu2@gmail.com>
2020-07-07 08:29:57 +1000

    add title to page

--- deploy.js
+++ deploy.js
@@ -19,21 +19,28 @@ fs.writeFileSync("index.html", lines);
 let templateLines = String(fs.readFileSync("per_page_template.html"));
 templateLines = templateLines.split(/\r?\n/g);
 let insertionLine = 0;
+let titleInsertionLine = 0;
 for (let i = 0; i < templateLines.length; i++) {
     if (templateLines[i].match(/insert markdown content here/)) {
         insertionLine = i;
+    }
+    if (templateLines[i].match(/\<title\>/)) {
+        titleInsertionLine = i;
     }
 }
 fs.rmdirSync("entries", { recursive: true });
 fs.mkdirSync("entries");
 for (let f of filesList) {
     let copy = JSON.parse(JSON.stringify(templateLines));
-    let selectedFile = String(fs.readFileSync("entries_md/" + f)).split(/\r?\n/g);
-    selectedFile.unshift(insertionLine + 1, 0);
+    let selectedFileLines = String(fs.readFileSync("entries_md/" + f)).split(/\r?\n/g);
+    selectedFileLines.unshift(insertionLine + 1, 0);// add 0 to args so that we splice away nothing
     copy.splice.apply(copy, selectedFile);
+    copy.splice.apply(copy, [0, titleInsertionLine + 1, selectedFileLines[0].slice(1)]);
     fs.mkdirSync(`entries/${f.slice(0, f.length - 3)}`);
     fs.writeFileSync(`entries/${f.slice(0, f.length - 3)}/index.html`, copy.join("\n"));
 }
+/*
 execSync("git add .")
 execSync("git commit -m 'auto-blog-update'");
 execSync("git push");
+*/
